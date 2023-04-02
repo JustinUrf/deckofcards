304105a6cb28c130ee4d0db4be0069eb07602ec5
Justin Lee <lee.justin001126@gmail.com>
2023-04-01 23:31:29 -0700

    add rookie in first hand with mulligans

--- deckofcards.py
+++ deckofcards.py
@@ -1,4 +1,5 @@
 import random
+import numpy as np
 import itertools
 
 class Card:
@@ -20,32 +21,62 @@ def generate_deck(numberOption, numberStarter):
   return cards
 
 
-cards = generate_deck(12, 14)
+# def brick_checker(numberStarter, firstOrSecond): Inneficient memory usage to create a deck multiple times. Instead made a function that asks for a deck as a parameter.
+#   deck = generate_deck(0, numberStarter)
+#   random.shuffle(deck)
+  
+#   if firstOrSecond == "first":
+#     startingHand = deck[0:5]
+#   else:
+#     startingHand = deck[0:6]
+  
+#   for i in range(len(startingHand)):
+#     if startingHand[i].type == "starter":
+#       return False
+    
+#   return True
 
-def shuffle(self):
-  if not len(self.temp):
-    return []
+
+
+def brick_checker_with_mulligan(deck, firstOrSecond):
+  newDeck = deck
+  np.random.shuffle(newDeck)
+  
+  if firstOrSecond == "first":
+    startingHand = newDeck[0:5]
+  else:
+    startingHand = newDeck[0:6]
+    
+  for i in range(len(startingHand)):
+    if startingHand[i].type == "starter":
+      return True
+  
+  np.random.shuffle(newDeck)
+  
+  if firstOrSecond == "first":
+    startingHand = newDeck[0:5]
+  else:
+    startingHand = newDeck[0:6]
+  
+  for i in range(len(startingHand)):
+    if startingHand[i].type == "starter":
+      return True
+  
+  return False
+
+deck = generate_deck(12,14)
+
+
+
   
 brickResult = {
   "brick": 0,
   "notBrick" : 0,
 }
 
-def brick_checker(numberStarter):
-  deck = generate_deck(0, numberStarter)
-  random.shuffle(deck)
-  
-  startingHand = deck[0:6]
-  
-  for i in range(len(startingHand)):
-    if startingHand[i].type == "starter":
-      return False
-    
-  return True
-
 for i in range(10000):
-  flag = brick_checker(8)
-  if flag == False:
+  flag = brick_checker_with_mulligan(deck, "first")
+  if flag == True:
     brickResult["notBrick"] += 1
   else:
     brickResult["brick"] += 1
